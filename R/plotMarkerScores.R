cbc592e779e18f9efbff642f50dea079c20bedbb
mahmoudibrahim <mahmoud.guc@gmail.com>
2019-06-25 12:57:43 +0200

    bug fixed

--- R/plotMarkerScores.R
+++ R/plotMarkerScores.R
@@ -16,8 +16,8 @@
 #' mm = getMarkers(gs,quant=0.99)
 #' plotMarkerScores(mm, gs)
 plotMarkerScores = function(mm, gs, markers = mm$markers, colors = c("#99999920","orangered4")) {
-  coloring = rep(colors[1],length(mm$gene_entropy))
-  coloring[which(names(mm$gene_entropy) %in% markers)] = colors[2]
+  coloring = rep(colors[1],length(mm$gene_shannon_index))
+  coloring[which(names(mm$gene_shannon_index) %in% markers)] = colors[2]
 
   par(mfrow = c(1,2))
   plot(mm$gene_shannon_index, mm$maxScaledSpecScore, col = coloring, pch = 19, xlab = "Specificity Shannon Index", ylab = "Max. Scaled Specificity Score")
